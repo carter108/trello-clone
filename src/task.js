5732040096c8b67533610aa4eeaf22708aa3facc
carter108 <nvtuong108@gmail.com>
2019-05-08 08:09:20 +0700

    Only allow drag forward

--- src/task.js
+++ src/task.js
@@ -1,14 +1,19 @@
-import React from 'react';
-import styled from 'styled-components';
-import { Draggable } from 'react-beautiful-dnd';
+import React from "react";
+import styled from "styled-components";
+import { Draggable } from "react-beautiful-dnd";
 
 const Container = styled.div`
   border: 1px solid lightgrey;
   border-radius: 2px;
   padding: 8px;
   margin-bottom: 8px;
-  background-color: ${props => props.isDragging ? 'lightgreen': 'white'};
   display: flex;
+  background-color: ${props =>
+    props.isDragDisabled
+      ? "lightgrey"
+      : props.isDragging
+      ? "lightgreen"
+      : "white"};
 `;
 
 const Handle = styled.div`
@@ -17,25 +22,28 @@ const Handle = styled.div`
   background-color: orange;
   margin-right: 8px;
   border-radius: 4px;
-`
+`;
 
 export default class Task extends React.Component {
   render() {
+    const isDragDisabled = this.props.task.id === "task-1";
     return (
-      <Draggable draggableId={this.props.task.id} index={this.props.index}>
-        {
-          (provided, snapshot) => (
-            <Container
-              {...provided.draggableProps}
-              ref={provided.innerRef}
-              isDragging={snapshot.isDragging}
-            >
+      <Draggable
+        draggableId={this.props.task.id}
+        index={this.props.index}
+        isDragDisabled={isDragDisabled}
+      >
+        {(provided, snapshot) => (
+          <Container
+            {...provided.draggableProps}
+            ref={provided.innerRef}
+            isDragging={snapshot.isDragging}
+          >
             <Handle {...provided.dragHandleProps} />
-              {this.props.task.content}
-            </Container>
-          )
-        }
+            {this.props.task.content}
+          </Container>
+        )}
       </Draggable>
-    )
+    );
   }
 }
